f494b07adf40c5ab43210d1d85e20256c4bb9bc1
Cayden Hutt <caydenhutt208@gmail.com>
2026-04-05 14:08:13 -0400

    updated main.py

--- main.py
+++ main.py
@@ -9,18 +9,29 @@ def func(a, t):
 t = np.linspace(0,10)
 
 # Streamlit stuff
-st.markdown("# Solution") # Labels the top of the site with 'Solution' in bold
+st.latex(r"y(t) = ae^{-2t/3} + \frac{2a-3}{3}te^{-2t/3}, 0\le t \le8, a>0") # Labels the top of the site with the function I am modeling 
 st.sidebar.markdown("# Variables") # Creates and Labels a sidebar with 'Variables' at the top in bold
 a = st.sidebar.slider( 
     "Select values of a", 
      1, 100
     )
 # the above code implements a value slider from 0,100 (integers) for a
-df = pd.DataFrame(
+df_1 = pd.DataFrame(
     {
         'input': t ,
         'output': func(a,t)
     }
 )
+# Plot to for 0<=t<=8
+st.line_chart(df_1,x='input' ,y='output', x_label='t', y_label='y(t)') # creates a line chart from columns in the DataFrame above
 
-st.line_chart(df,x='input' ,y='output', x_label='t', y_label='y(t)') # creates a line chart from columns in the dataframe above
+t_2 = np.linspace(0,15)
+df_2 = pd.DataFrame(
+    {
+        'input': t_2,
+        'output': func(a, t_2) 
+    }
+)
+st.latex(r"y(t) = ae^{-2t/3} + \frac{2a-3}{3}te^{-2t/3}, 0\le t \le15, a>0") 
+# Plot to for 0<=t<=15
+st.line_chart(df_2,x='input' ,y='output', x_label='t', y_label='y(t)') # creates a line chart from columns in the 'DataFrame above
