e72f781a5211897f588ebad272bf937c5d57d5f6
Cayden Hutt <caydenhutt208@gmail.com>
2026-04-05 00:47:02 -0400

    Update main.py and README

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # Solution Modeling
 I am modeling the general solution to a Second Order, Linear, Homogenous, 
 constant coefficient Differential equation for varying values of a using
-streamlit and numpy as the backbone.
+streamlit, numpy, and pandas
 
 # Libraries
 1. Numpy
 
 2. Streamlit
+
+3. Pandas
+
+# Purpose:
+I wanted to get some experience using streamlit before I began implementng
+my option pricing project. I have been aware of the streamlit library for
+roughly a year now, and have made no attempts to become familiar with it.
+However very recently I seem to have learned how to code, so I'd like to 
+play with it a bit.
--- main.py
+++ main.py
@@ -4,19 +4,23 @@ import pandas as pd
 # Plotting for my solution to homework 5 part c
 
 def func(a, t):
-    y = a*np.exp((-2/3)*t) + ((2*a + 3)/3) * t * np.exp((-2/3)*t)
+    y = a*np.exp((-2/3)*t) + ((2*a + 3)/3) * t * np.exp((-2/3)*t) # Solution to the DE(found by hand)
     return y
-t = np.linspace(0,8)
-a = st.sidebar.slider(
-    'Select values of a', 
-     0, 100
+t = np.linspace(0,10)
+
+# Streamlit stuff
+st.markdown("# Solution") # Labels the top of the site with 'Solution' in bold
+st.sidebar.markdown("# Variables") # Creates and Labels a sidebar with 'Variables' at the top in bold
+a = st.sidebar.slider( 
+    "Select values of a", 
+     1, 100
     )
+# the above code implements a value slider from 0,100 (integers) for a
 df = pd.DataFrame(
     {
         'input': t ,
         'output': func(a,t)
     }
 )
-print(df.head())
-st.line_chart(df,x='input' ,y='output', x_label='t', y_label='y(t)')
 
+st.line_chart(df,x='input' ,y='output', x_label='t', y_label='y(t)') # creates a line chart from columns in the dataframe above
